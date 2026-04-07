ca1712455965b554f89c97b781478671b4e11b21
Michael Thole <57376542+mthole@users.noreply.github.com>
2026-04-07 12:19:00 -0700

    feat: make FrostedUI a dynamic framework (#10)

--- Package.swift
+++ Package.swift
@@ -7,14 +7,12 @@ let package = Package(
     name: "FrostedUI",
     platforms: [.iOS(.v17)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FrostedUI",
+            type: .dynamic,
             targets: ["FrostedUI"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "FrostedUI",
             dependencies: [],
